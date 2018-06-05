d620062dc9f6a482f952831321b546e94d71be37
cthach010 <cthach010@gmail.com>
2018-06-05 19:22:46 -0400

    watermeloan

---
+++ app/data/friends.js
@@ -0,0 +1,164 @@
+var friendList = [
+{
+    name: "Wilbur",
+    photo: "https://www.50-best.com/images/cute_animal_pictures/thumbs/1-5.jpg",
+    scores: [
+        '5',
+        '5',
+        '5',
+        '5',
+        '5',
+        '5',
+        '5',
+        '5',
+        '5',
+        '5'
+    ]
+},
+{
+    name: "Dory",
+    photo: "http://ichef.bbci.co.uk/wwfeatures/wm/live/1280_640/images/live/p0/37/pq/p037pq3k.jpg",
+    scores: [
+        '2',
+        '3',
+        '3',
+        '1',
+        '3',
+        '3',
+        '2',
+        '2',
+        '3',
+        '2'
+    ]
+},
+{
+    name: "Thumper",
+    photo: "https://http2.mlstatic.com/conejos-enanos-de-orejas-caidas-holland-lop-D_NQ_NP_17235-MLA20134604236_072014-F.jpg",
+    scores:[
+        '4',
+        '5',
+        '5',
+        '1',
+        '1',
+        '2',
+        '5',
+        '4',
+        '3',
+        '4'
+    ]
+},
+{
+    name: "Kiara",
+    photo: "https://www.hd-wallpapersdownload.com/script/bulk-upload/images-of-a-baby-lion-cub.jpgg",
+    scores: [
+        '5',
+        '1',
+        '2',
+        '1',
+        '3',
+        '4',
+        '1',
+        '3',
+        '3',
+        '4'
+    ]
+},
+{
+    name: "Pascal",
+    photo: "https://www.cbreptile.com/wp-content/uploads/2017/08/ambilobe-panther-chameleon-breeder.jpg",
+    scores: [
+        '1',
+        '1',
+        '5',
+        '5',
+        '5',
+        '5',
+        '5',
+        '5',
+        '5',
+        '1'
+    ]
+},
+{
+    name: "Maximus",
+    photo: "https://awakenfromthedream.files.wordpress.com/2015/07/horse.jpg",
+    scores: [
+        '1',
+        '1',
+        '3',
+        '1',
+        '3',
+        '4',
+        '5',
+        '1',
+        '3',
+        '5'
+    ]
+},
+{
+    name: "Remy",
+    photo: "https://pixfeeds.com/images/animals/mice/320-523059108-black-mouse.jpg",
+    scores: [
+        '5',
+        '4',
+        '4',
+        '2',
+        '4',
+        '4',
+        '3',
+        '3',
+        '3',
+        '3'
+    ]
+},
+{
+    name: "Dumbo",
+    photo: "http://www.pxleyes.com/images/contests/real-life-cartoon/fullsize/real-life-cartoon-57ac4d842e281_hires.jpg",
+    scores: [
+        '5',
+        '2',
+        '3',
+        '1',
+        '5',
+        '1',
+        '3',
+        '1',
+        '1',
+        '5'
+    ]
+},
+{
+    name: "Marie",
+    photo: "https://i.pinimg.com/originals/67/06/0f/67060f68a7e849142d86b715fc897d29.jpg",
+    scores: [
+        '4',
+        '5',
+        '5',
+        '5',
+        '5',
+        '5',
+        '5',
+        '5',
+        '5',
+        '5'
+    ]
+},
+{
+    name: "Eeyore",
+    photo: "https://cdn.images.express.co.uk/img/dynamic/80/590x/secondary/donkey-drain-218215.jpgg",
+    scores: [
+        '1',
+        '1',
+        '4',
+        '1',
+        '1',
+        '1',
+        '1',
+        '1',
+        '1',
+        '1'
+    ]
+}
+];
+
+module.exports = friendList;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,51 @@
+
+var friendList = require("../data/friends");
+
+
+module.exports = function(app) {
+
+  app.get("/api/friends", function(req, res) {
+    res.json(friendList);
+  });
+
+
+  app.post("/api/friends", function(req, res) {
+
+  });
+
+    var bestFriend = {
+        name: "",
+        photo: "",
+        scores: 1000
+  };
+
+  var userData 	= req.body;
+  var userName 	= userData.name;
+  var userPhoto 	= userData.photo;
+  var userScores 	= userData.scores;
+
+  var totalDifference = [];
+
+
+  for  (var i=0; i< friends.length; i++) {
+
+    console.log(friends[i].name);
+    totalDifference = 0;
+
+
+    for (var j=0; j< friends[i].scores[j]; j++){
+
+      totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
+
+      if (totalDifference <= bestFriend.totalDifference){
+
+        bestFriend.name = friends[i].name;
+        bestFriend.photo = friends[i].photo;
+        bestFriend.friendDifference = totalDifference;
+      }
+    }
+  }
+	friendList.push(userData);
+ 
+		res.json(bestFriend);
+	};
